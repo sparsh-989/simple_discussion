9cc82e74276a908c625ec50a51f8c043d1ecc652
sparsh-989 <Ishunitk989@gmail.com>
2024-03-10 21:17:30 +0530

    changed profanity

--- app/controllers/simple_discussion/forum_threads_controller.rb
+++ app/controllers/simple_discussion/forum_threads_controller.rb
@@ -49,7 +49,7 @@ class SimpleDiscussion::ForumThreadsController < SimpleDiscussion::ApplicationCo
     
 
   # Apply language filter
-    filtered_body = LanguageFilter::Filter.new(matchlist: :profanity, replacement: :stars).sanitize(current_body)
+    filtered_body = LanguageFilter::Filter.new(matchlist: :sex, replacement: :stars).sanitize(current_body)
 
   # Update the body with filtered content
     first_post.body = filtered_body
